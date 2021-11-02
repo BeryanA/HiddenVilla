3f6bf7ddcee9ecec58e7f252527d882ff4f8b2c9
Beryan <asberyan@gmail.com>
2021-11-02 22:34:51 +0400

    Add component AmenetyRoom

---
+++ HiddenVilla_Server/Model/BlazorAmenity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HiddenVilla_Server.Model
+{
+    public class BlazorAmenity
+    {
+        public int Id { get; set; }
+        public string AmenityName { get; set; }
+        public string Description { get; set; }
+    }
+}
